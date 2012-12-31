8fb18c3e50b1d17a96152cb0a7e6f3daece68fa7
zml2008 <zach@zachsthings.com>
2012-12-31 08:53:11 -0800

    Merged SimpleEventManager into EventManager, made all methods static
    Signed-off-by: zml2008 <zach@zachsthings.com>

--- src/main/java/com/zachsthings/onevent/EventManager.java
+++ src/main/java/com/zachsthings/onevent/EventManager.java
@@ -16,30 +16,125 @@
 
 package com.zachsthings.onevent;
 
-public interface EventManager {
-    /**
-     * Calls an event with the given details
-     *
-     * @param event Event details
-     * @return Called event
-     */
-    public <T extends Event> T callEvent(T event);
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class EventManager {
+    private static final Logger LOGGER = Logger.getLogger(EventManager.class.getCanonicalName());
+
+    private EventManager() {
+    }
+
+    public static <T extends Event> T callEvent(T event) {
+        HandlerList handlers = event.getHandlers();
+        ListenerRegistration[] listeners = handlers.getRegisteredListeners();
+
+        if (listeners != null) {
+            for (ListenerRegistration listener : listeners) {
+                try {
+                    if (!event.isCancelled() || listener.getOrder().ignoresCancelled()) {
+                        listener.getExecutor().execute(event);
+                    }
+                } catch (Throwable ex) {
+                    LOGGER.log(Level.SEVERE, "Could not pass event " + event.getEventName() + " to " + listener.getOwner().getClass().getName(), ex);
+                }
+            }
+        }
+        return event;
+    }
+
+    public static void registerEvents(Listener listener, Object owner) {
+        for (Map.Entry<Class<? extends Event>, Set<ListenerRegistration>> entry : createRegisteredListeners(listener, owner).entrySet()) {
+            Class<? extends Event> delegatedClass = getRegistrationClass(entry.getKey());
+            if (!entry.getKey().equals(delegatedClass)) {
+                LOGGER.severe("Plugin attempted to register delegated event class " + entry.getKey() + ". It should be using " + delegatedClass + "!");
+                continue;
+            }
+            getEventListeners(delegatedClass).registerAll(entry.getValue());
+        }
+    }
+
+    public static void registerEvent(Class<? extends Event> event, Order priority, EventExecutor executor, Object owner) {
+        getEventListeners(event).register(new ListenerRegistration(executor, priority, owner));
+    }
 
     /**
-     * Registers all the events in the given listener class
+     * Returns the specified event type's HandlerList
      *
-     * @param listener Listener to register
-     * @param owner Plugin to register
+     * @param type EventType to lookup
+     * @return HandlerList The list of registered handlers for the event.
      */
-    public void registerEvents(Listener listener, Object owner);
+    private static HandlerList getEventListeners(Class<? extends Event> type) {
+        try {
+            Method method = getRegistrationClass(type).getDeclaredMethod("getHandlerList");
+            method.setAccessible(true);
+            return (HandlerList) method.invoke(null);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e.toString());
+        }
+    }
 
-    /**
-     * Registers the specified executor to the given event class
-     *
-     * @param event Event type to register
-     * @param priority Priority to register this event at
-     * @param executor EventExecutor to register
-     * @param owner Plugin to register
-     */
-    public void registerEvent(Class<? extends Event> event, Order priority, EventExecutor executor, Object owner);
+    private static Class<? extends Event> getRegistrationClass(Class<? extends Event> clazz) {
+        try {
+            clazz.getDeclaredMethod("getHandlerList");
+            return clazz;
+        } catch (NoSuchMethodException e) {
+            if (clazz.getSuperclass() != null && !clazz.getSuperclass().equals(Event.class) && Event.class.isAssignableFrom(clazz.getSuperclass())) {
+                return getRegistrationClass(clazz.getSuperclass().asSubclass(Event.class));
+            } else {
+                throw new IllegalArgumentException("Unable to find handler list for event " + clazz.getName());
+            }
+        }
+    }
+
+    public static Map<Class<? extends Event>, Set<ListenerRegistration>> createRegisteredListeners(final Listener listener, Object plugin) {
+        Map<Class<? extends Event>, Set<ListenerRegistration>> ret = new HashMap<Class<? extends Event>, Set<ListenerRegistration>>();
+        Method[] methods;
+        try {
+            methods = listener.getClass().getDeclaredMethods();
+        } catch (NoClassDefFoundError e) {
+            LOGGER.severe("Plugin " + plugin.getClass().getSimpleName() + " is attempting to register event " + e.getMessage() + ", which does not exist. Ignoring events registered in " + listener.getClass());
+            return ret;
+        }
+        for (final Method method : methods) {
+            final EventHandler eh = method.getAnnotation(EventHandler.class);
+            if (eh == null) {
+                continue;
+            }
+            final Class<?> checkClass = method.getParameterTypes()[0];
+            Class<? extends Event> eventClass;
+            if (!Event.class.isAssignableFrom(checkClass) || method.getParameterTypes().length != 1) {
+                LOGGER.severe("Wrong method arguments used for event type registered");
+                continue;
+            } else {
+                eventClass = checkClass.asSubclass(Event.class);
+            }
+            method.setAccessible(true);
+            Set<ListenerRegistration> eventSet = ret.get(eventClass);
+            if (eventSet == null) {
+                eventSet = new HashSet<ListenerRegistration>();
+                ret.put(eventClass, eventSet);
+            }
+            eventSet.add(new ListenerRegistration(new EventExecutor() {
+
+                public void execute(Event event) throws EventException {
+                    try {
+                        if (!checkClass.isAssignableFrom(event.getClass())) {
+                            throw new EventException("Wrong event type passed to registered method");
+                        }
+                        method.invoke(listener, event);
+                    } catch (Throwable t) {
+                        throw new EventException(t);
+                    }
+                }
+
+            }, eh.order(), plugin));
+        }
+        return ret;
+    }
 }
--- src/test/java/com/zachsthings/onevent/EventManagerTest.java
+++ src/test/java/com/zachsthings/onevent/EventManagerTest.java
@@ -1,37 +1,32 @@
 package com.zachsthings.onevent;
 
-import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 /**
  *
- * Test for SimpleEventManager
+ * Test for the EventManager
  */
-public class SimpleEventManagerTest {
-    protected static final Logger LOGGER = Logger.getLogger(SimpleEventManagerTest.class.getCanonicalName());
+public class EventManagerTest {
 
 	@Test
 	public void testEventCalling() {
-        final SimpleEventManager eventManager = new SimpleEventManager(LOGGER);
 		final TestListener testListener = new TestListener();
-		eventManager.registerEvents(testListener, this);
-		eventManager.callEvent(new TestEvent());
+		EventManager.registerEvents(testListener, this);
+		EventManager.callEvent(new TestEvent());
 		assertTrue(testListener.hasBeenCalled());
         HandlerList.unregisterAll();
 	}
 
     @Test
     public void testSubEventCalling() {
-        final EventManager eventManager = new SimpleEventManager(LOGGER);
         final TestSubListener testListener = new TestSubListener();
-        eventManager.registerEvents(testListener, this);
-        eventManager.callEvent(new TestEvent());
-        eventManager.callEvent(new TestSubEvent());
+        EventManager.registerEvents(testListener, this);
+        EventManager.callEvent(new TestEvent());
+        EventManager.callEvent(new TestSubEvent());
 
         assertEquals(2, testListener.getParentCallCount());
         assertEquals(1, testListener.getChildCallCount());
@@ -41,17 +36,16 @@ public class SimpleEventManagerTest {
 
 	@Test
 	public void testEventPriorities() {
-        final EventManager eventManager = new SimpleEventManager(LOGGER);
 		final List<Order> calledOrders = new ArrayList<Order>();
 
 		for (final Order order : Order.values()) {
-			eventManager.registerEvent(TestEvent.class, order, new EventExecutor() {
+			EventManager.registerEvent(TestEvent.class, order, new EventExecutor() {
 				public void execute(Event event) throws EventException {
 					calledOrders.add(order);
 				}
 			}, this);
 		}
-		eventManager.callEvent(new TestEvent());
+		EventManager.callEvent(new TestEvent());
 
 		assertEquals(calledOrders.size(), Order.values().length);
 		for (Order order : Order.values()) {
